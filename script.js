5958cb2bd6b32d7d38a99388e92422c227bc3cb2
syl59 <ygrec95@yahoo.fr>
2023-03-21 16:25:35 +0100

    Ajout des fichiers au dépot

---
+++ script.js
@@ -0,0 +1,18 @@
+var a="John";
+
+function coucou(){
+    console.log("Coucou");
+}
+
+console.log(window);
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+console.log(window.outerWidth);
+
+console.log(window);
+console.log(innerWidth);
+console.log(innerHeight);
+console.log(outerHeight);
+console.log(outerWidth);
+
